b578d9f106182479fc13645bd44031eb2c3583a5
maayanalbert <maayan.albert@gmail.com>
2023-09-16 09:29:49 -0700

    Added full bleed bottom buttons

--- src/components/SpacesButtons.tsx
+++ src/components/SpacesButtons.tsx
@@ -8,52 +8,12 @@ import {
 import { ArrowLeftIcon, XMarkIcon } from "@heroicons/react/24/solid"
 
 export default function SpacesButtons() {
-  const { curSpace } = useSpacesContext()
-  const isMobile = useIsMobile()
-
   return (
-    <div className="absolute w-full flex justify-start items-start sm:p-28 sm:pb-32 p-8 h-[20%] bottom-0">
-      <div className="flex flex-row items-center justify-between sm:gap-3 gap-3 relative">
-        {curSpace && (
-          <div
-            className="absolute right-full"
-            style={{
-              right: isMobile ? undefined : "calc(100% + .75rem)",
-              left: isMobile ? "0" : undefined,
-              top: isMobile ? "calc(100% + .75rem)" : undefined,
-            }}
-          >
-            <BackSelectable />
-          </div>
-        )}
+    <div className="absolute w-full flex sm:justify-start justify-center sm:p-28 sm:pb-32 p-8 h-[20%] bottom-0">
+      <div className="flex flex-row items-center justify-between gap-3 relative sm:w-fit w-full">
         <SpaceSelectable space="WORK" />
         <SpaceSelectable space="CONTACT" />
         <SpaceSelectable space="LOCATION" />
-      </div>
-    </div>
-  )
-}
-
-function BackSelectable() {
-  const { curSpace, setCurSpace } = useSpacesContext()
-
-  const onPress = () => {
-    setCurSpace()
-  }
-
-  return (
-    <div
-      className={`flex justify-start items-start sm:py-1.5 py-1 cursor-pointer relative 
-      sm:w-10 w-10 text-sm sm:text-base group`}
-      onClick={onPress}
-    >
-      <p
-        className={`top-0 absolute h-full ${curSpace ? "w-full" : "w-0"}
-           transition-all ease-out group-hover:w-0`}
-        style={{ backgroundColor: "black" }}
-      />
-      <div className="flex h-full w-full justify-center items-center">
-        <ArrowLeftIcon className="h-6 w-6" />
       </div>
     </div>
   )
@@ -72,7 +32,7 @@ function SpaceSelectable({ space }: SpaceSelectableProps) {
   return (
     <div
       className={`text-center py-1.5 cursor-pointer relative 
-      sm:w-28 w-20 :text-base group`}
+      sm:w-28 w-full :text-base group`}
       onClick={onPress}
     >
       <p
