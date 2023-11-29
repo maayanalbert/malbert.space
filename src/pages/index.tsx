cbb236189f5b30fe1c9ff3d4164e63af23d967cb
maayanalbert <maayan.albert@gmail.com>
2023-11-29 16:30:41 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,9 +10,7 @@ export default function Home() {
 
   return (
     <div className="h-full w-full">
-      <div
-        className="absolute bottom-[50%] sm:bottom-[calc(50%-44px] sm:pl-28 pl-8"
-      >
+      <div className="absolute bottom-[50%] sm:bottom-[calc(50%-44px)] sm:pl-28 pl-8">
         <p
           className="font-semibold sm:text-9xl text-5xl sm:leading-normal leading-tight"
           style={{ fontFamily: "Helvetica Neue" }}
