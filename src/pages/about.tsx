5632f3be18aabc4d01f15e53e712abae08c4bd62
maayanalbert <maayan.albert@gmail.com>
2023-11-30 05:56:01 -0500

    just info

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -62,3 +62,57 @@ export default function About() {
     </div>
   )
 }
+
+function AboutInfo() {
+  return (
+    <p className="text-lg">
+      I studied Design & CS at{" "}
+      <a
+        // href={link}
+        target="_blank"
+        className="hover:underline cursor-pointer"
+        style={{ color: getPageColor("ABOUT") }}
+      >
+        Carnegie Mellon
+      </a>
+      , where I wrote my thesis on{" "}
+      <a
+        // href={link}
+        target="_blank"
+        className="hover:underline cursor-pointer"
+        style={{ color: getPageColor("ABOUT") }}
+      >
+        Authoring Environments
+      </a>
+      .<br />
+      I've worked at{" "}
+      <a
+        // href={link}
+        target="_blank"
+        className="hover:underline cursor-pointer"
+        style={{ color: getPageColor("ABOUT") }}
+      >
+        Apple
+      </a>
+      ,{" "}
+      <a
+        // href={link}
+        target="_blank"
+        className="hover:underline cursor-pointer"
+        style={{ color: getPageColor("ABOUT") }}
+      >
+        Google
+      </a>
+      , and a few startups, and am now building the voice of the void with{" "}
+      <a
+        // href={link}
+        target="_blank"
+        className="hover:underline cursor-pointer"
+        style={{ color: getPageColor("ABOUT") }}
+      >
+        Eve
+      </a>
+      .
+    </p>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { ReactNode, useEffect } from "react"
 
 /**
  * A wrapper for the main page
@@ -16,6 +16,24 @@ export default function Home() {
       >
         Hi, I'm Maayan
       </p>
+    </div>
+  )
+}
+
+interface Props {
+  info: ReactNode
+}
+
+export function HomeLayout(){
+  return (
+    <div className="h-[85%] w-full flex flex-col sm:items-start items-center justify-center sm:pl-28">
+      <p
+        className="font-semibold sm:text-9xl text-5xl sm:leading-normal leading-tight sm:text-left text-center"
+        style={{ fontFamily: "Helvetica Neue" }}
+      >
+        Hi, I'm Maayan
+      </p>
+      <div
     </div>
   )
 }
