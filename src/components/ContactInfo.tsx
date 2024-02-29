e931a67c78638c7dfdfe5942c62a791645d92b57
maayanalbert <maayan.albert@gmail.com>
2024-02-29 12:19:09 -0500

    Update ContactInfo.tsx

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -2,43 +2,26 @@ import { useEffect } from "react"
 import NavButtons from "@/components/NavButtons"
 import { Item, Row } from "@/components/Item"
 import { getPageColor } from "@/pageHelpers"
+import TextLink from "./TextLink"
 
 export default function ContactInfo() {
   return (
     <div className="flex sm:flex-row flex-col gap-5">
-      <p>
-        Email:{" "}
-        <a
-          href="mailto:maayan@eve.space"
-          target="_blank"
-          className="hover:underline cursor-pointer"
-          style={{ color: getPageColor("CONTACT") }}
-        >
-          maayan@eve.space
-        </a>
-      </p>
-      <p>
-        Twitter:{" "}
-        <a
-          href="https://twitter.com/_maayanster"
-          target="_blank"
-          className="hover:underline cursor-pointer"
-          style={{ color: getPageColor("CONTACT") }}
-        >
-          @_maayanster
-        </a>
-      </p>
-      <p>
-        Github:{" "}
-        <a
-          href="https://github.com/maayanalbert"
-          target="_blank"
-          className="hover:underline cursor-pointer"
-          style={{ color: getPageColor("CONTACT") }}
-        >
-          @maayanalbert
-        </a>
-      </p>
+      <TextLink
+        text="maayan@eve.space"
+        href="mailto:maayan@eve.space"
+        page="CONTACT"
+      />
+      <TextLink
+        text="twitter.com/_maayanster"
+        href="https://twitter.com/_maayanster"
+        page="CONTACT"
+      />
+      <TextLink
+        text="github.com/maayanalbert"
+        href="https://github.com/maayanalbert"
+        page="CONTACT"
+      />
     </div>
   )
 }
